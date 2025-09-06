a95702074fa2b5a5d0db0598afc2f547641d80a4
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-09-06 04:32:23 +0300

    fixed: testing utilities and concurrency

--- rkrpc-core/src/jvmTest/kotlin/MainTest.kt
+++ rkrpc-core/src/jvmTest/kotlin/MainTest.kt
@@ -1,33 +1,27 @@
 package net.lsafer.rkrpc.test
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.job
 import kotlinx.coroutines.test.runTest
 import kotlinx.rpc.krpc.serialization.json.json
 import kotlinx.rpc.registerService
 import kotlinx.rpc.withService
 import net.lsafer.rkrpc.configureSubClient
-import net.lsafer.rkrpc.reverseServer
-import net.lsafer.rkrpc.reverseClient
-import net.lsafer.rkrpc.test.util.createServerClientTest
 import net.lsafer.rkrpc.newSubClient
+import net.lsafer.rkrpc.reverseClient
+import net.lsafer.rkrpc.reverseServer
+import net.lsafer.rkrpc.test.util.childScope
+import net.lsafer.rkrpc.test.util.createServerClientTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 open class MainTest {
     @Test
     fun `simple client server send receive`() = runTest {
-        val scope = CoroutineScope(coroutineContext + Dispatchers.IO + SupervisorJob())
-
         createServerClientTest(
-            coroutineScope = scope,
-            serverBlock = {
+            serverBlock = { scope ->
                 val rClient = reverseClient(scope) { serialization { json() } }
                 val nameService = rClient.withService<ClientNameService>()
 
@@ -45,7 +39,7 @@ open class MainTest {
                     }
                 }
             },
-            clientBlock = { client ->
+            clientBlock = { client, scope ->
                 val rServer = client.reverseServer(scope) {
                     rpcConfig { serialization { json() } }
                     registerService<ClientNameService> {
@@ -72,17 +66,12 @@ open class MainTest {
                 rServer.close()
             },
         )
-
-        scope.coroutineContext.job.join()
     }
 
     @Test
     fun `simple factory and withScope usage`() = runTest {
-        val scope = CoroutineScope(coroutineContext + Dispatchers.IO + SupervisorJob())
-
         createServerClientTest(
-            coroutineScope = scope,
-            serverBlock = {
+            serverBlock = { scope ->
                 configureSubClient(scope) {
                     rpcConfig { serialization { json() } }
                     registerService<CounterService> {
@@ -96,10 +85,10 @@ open class MainTest {
                     }
                 }
             },
-            clientBlock = { client ->
-                val client1 = client.newSubClient(scope) { serialization { json() } }
-                val client2 = client.newSubClient(scope) { serialization { json() } }
-                val client3 = client.newSubClient(scope) { serialization { json() } }
+            clientBlock = { client, scope ->
+                val client1 = client.newSubClient(scope.childScope()) { serialization { json() } }
+                val client2 = client.newSubClient(scope.childScope()) { serialization { json() } }
+                val client3 = client.newSubClient(scope.childScope()) { serialization { json() } }
 
                 val service1 = client1.withService<CounterService>()
                 val service2 = client2.withService<CounterService>()
@@ -120,10 +109,10 @@ open class MainTest {
                 assertEquals(2, m)
                 assertEquals(2, n)
 
-                client.close()
+                client1.close()
+                client2.close()
+                client3.close()
             },
         )
-
-        scope.coroutineContext.job.join()
     }
 }
--- rkrpc-core/src/jvmTest/kotlin/util/createServerClientTest.kt
+++ rkrpc-core/src/jvmTest/kotlin/util/createServerClientTest.kt
@@ -1,7 +1,10 @@
 package net.lsafer.rkrpc.test.util
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.job
 import kotlinx.rpc.krpc.KrpcTransport
 import kotlinx.rpc.krpc.KrpcTransportMessage
 import kotlinx.rpc.krpc.client.InitializedKrpcClient
@@ -10,6 +13,8 @@ import kotlinx.rpc.krpc.rpcClientConfig
 import kotlinx.rpc.krpc.rpcServerConfig
 import kotlinx.rpc.krpc.serialization.json.json
 import kotlinx.rpc.krpc.server.KrpcServer
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 private class TestTransport(
     val inChannel: Channel<KrpcTransportMessage>,
@@ -26,27 +31,44 @@ private class TestTransport(
 }
 
 suspend fun createServerClientTest(
-    coroutineScope: CoroutineScope,
-    serverBlock: suspend KrpcServer.() -> Unit,
-    clientBlock: suspend (KrpcClient) -> Unit,
+    serverBlock: suspend KrpcServer.(CoroutineScope) -> Unit,
+    clientBlock: suspend (KrpcClient, CoroutineScope) -> Unit,
 ) {
     val inChannel = Channel<KrpcTransportMessage>()
     val outChannel = Channel<KrpcTransportMessage>()
     var server: KrpcServer? = null
     var client: KrpcClient? = null
+    val serverJob = SupervisorJob()
+    val clientJob = SupervisorJob()
+    val serverScope = CoroutineScope(serverJob)
+    val clientScope = CoroutineScope(clientJob)
     try {
         server = object : KrpcServer(
             rpcServerConfig { serialization { json() } },
-            TestTransport(inChannel, outChannel, coroutineScope),
+            TestTransport(inChannel, outChannel, serverScope),
         ) {}
-        serverBlock(server)
+        serverBlock(server, serverScope)
         client = object : InitializedKrpcClient(
             rpcClientConfig { serialization { json() } },
-            TestTransport(outChannel, inChannel, coroutineScope),
+            TestTransport(outChannel, inChannel, clientScope),
         ) {}
-        clientBlock(client)
+        clientBlock(client, clientScope)
+        serverJob.complete()
+        clientJob.complete()
+    } catch (e: Throwable) {
+        serverJob.cancel()
+        clientJob.cancel()
+        throw e
     } finally {
         server?.close()
         client?.close()
+        serverJob.join()
+        clientJob.join()
     }
 }
+
+fun CoroutineScope.supervisorChildScope(extra: CoroutineContext = EmptyCoroutineContext): CoroutineScope =
+    CoroutineScope(coroutineContext + SupervisorJob(coroutineContext.job) + extra)
+
+fun CoroutineScope.childScope(extra: CoroutineContext = EmptyCoroutineContext): CoroutineScope =
+    CoroutineScope(coroutineContext + Job(coroutineContext.job) + extra)
